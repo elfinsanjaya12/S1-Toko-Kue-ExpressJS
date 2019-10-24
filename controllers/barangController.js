c02cf7fddb2f9323b9a1eddf091bad2f35d219e3
Elfin Sanjaya <elfinsanjaya1207@gmail.com>
2019-10-24 14:48:53 +0700

    add logic kode barang automatic

--- controllers/barangController.js
+++ controllers/barangController.js
@@ -3,6 +3,7 @@ const Op = require("sequelize").Op
 
 exports.viewBarang = async (req, res) => {
   const barang = await Barang.findAll()
+
   res.render("admin/barang/view_barang", {
     title: "Barang",
     barang: barang
@@ -10,11 +11,41 @@ exports.viewBarang = async (req, res) => {
 }
 
 exports.actionCreate = async (req, res) => {
-  const { kode_barang, nama_barang, satuan, harga } = req.body
+  const {
+    nama_barang,
+    satuan,
+    harga
+  } = req.body
+
 
   try {
+    /* membuat kode barang automatic */
+    const barang = await Barang.findAll({
+      order: [
+        ['id', 'DESC'],
+      ],
+    })
+
+    // ambil data kode barang paling akhir dengan desc
+    const data = barang[0].kode_barang;
+    var reg = /\d/g;
+    var match = data.match(reg);
+    let tampung = '';
+    if (match.length > 0) {
+      for (let i = 0; i < match.length; i++) {
+        tampung += match[i];
+      }
+    }
+    var auto = Number(tampung) + 1;
+    var kode = "B-OO";
+    let code_auto = kode + auto;
+    /* akhir kode barang automatic */
+
     await Barang.create({
-      kode_barang, nama_barang, satuan, harga
+      kode_barang: code_auto,
+      nama_barang,
+      satuan,
+      harga
     })
     res.redirect("/admin/barang");
   } catch (err) {
